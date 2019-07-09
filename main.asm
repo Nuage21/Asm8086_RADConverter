b4f1cafc68ea07aee1702cb304c9547f73f4c836
hbFree <ih_beldjoudi@esi.dz>
2019-07-09 19:51:17 +0100

    [INIT]

---
+++ main.asm
@@ -0,0 +1,130 @@
+
+; You may customize this and other start-up templates; 
+; The location of this template is c:\emu8086\inc\0_com_template.txt
+
+org 100h
+
+.data
+
+welcomeStr      db "Hi welcome to the RANUM_CONVERTER __ completely written with asm8086$"
+enterStr        db "Please enter a number to be convert: $"
+romanToArabStr  db "Conversion from roman to arab digits...$"
+arabToRomanStr  db "Conversion from arab to roman digits...$"
+endlStr         db 0ah, 0dh, '$'
+
+; ROMAN DIGTS
+
+CROMAN_DIGITS db 'I', 'V', 'X', 'L', 'C', 'D', 'M' ; Capital 7 chars
+LROMAN_DIGITS db 'i', 'v', 'x', 'l', 'c', 'd', 'm' ; little chars  
+
+
+input      db "$$$$$$$$$$" ; read stream
+
+ends
+
+; defines some macros
+
+macro print_str msg ; print a string
+    push ax
+    push dx
+    mov ah, 09h
+    mov dx, offset msg
+    int 21h
+    pop dx
+    pop ax
+endm 
+
+macro endl 
+    print_str endlStr
+endm
+
+.code
+
+mov ax, @data
+mov ds, ax
+
+print_str welcomeStr
+endl 
+print_str enterStr
+endl
+
+mov ax, 'C'
+push ax
+call isRomanDigit
+cmp al, 00h
+jne cmp1
+print_str romanToArabStr
+cmp1:
+print_str arabToRomanStr  
+
+;mov bx, 09h
+;lea di, input   
+
+;call get_str
+;endl
+;print_str input
+ 
+mov ah, 4ch
+int 21h
+
+get_str proc near   
+                    ; Reads and stores a string.                            
+                    ; input: DI offset of string                            
+                    ; output: DI offset of string                           
+                    ; bx number of characters read                     
+    push ax
+    push di 
+    cld             ; process from left ->
+    xor bx, bx      ; bx <-- 00h
+    mov ah, 1       ; input char function code
+    int 21h
+    
+    while1:
+    
+    cmp al, 0dh     ; is it a carriage return ?
+    je end_while1
+    cmp al, 9h      ; backspace ?
+    jne else1
+    dec di          ; move str ptr back 
+    dec bx          ; charCounter--
+    jmp read        ;read another char
+    else1:
+    stosb           ; store char in string
+    inc bx
+    read:
+    int 21h         ; read a char into al
+    jmp while1      ; loop again
+    end_while1:
+    pop di   
+    pop ax
+    ret                                               
+
+get_str endp      
+
+isRomanDigit proc near
+    ; enter a char through pile 
+    ; return ah, 00h means it's a roman 
+    push bp
+    mov bp, sp
+    mov dx, [bp+4] 
+    mov ax, 1
+    lea bx, LROMAN_DIGITS
+    mov cx, 7  
+    while2:
+    mov dh,[bx]
+    cmp dl, dh
+    je yesRoman
+    inc bx
+    loop while2
+    pop bp 
+    ret
+    yesRoman:
+    mov al, 00h
+    pop bp
+    ret
+isRomanDigit endp
+
+ends
+
+
+
